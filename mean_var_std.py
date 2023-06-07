50ef6934fcb668ba8a047cbd848d0d468b816f8e
Shyamsys <82777789+Shyamsys@users.noreply.github.com>
2023-06-07 19:13:20 +0530

    Add files via upload

---
+++ mean_var_std.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def calculate(list_numbers):
+
+    # If the list containing less than 9 elements it should raise a ValueError
+    if len(list_numbers) < 9:
+        raise ValueError("List must contain nine numbers.")
+
+    # Convert the list into a 3 x 3 Numpy array
+    np_matrix = np.array(list_numbers).reshape((3, 3))
+
+    # Dictionary with the statistics to evaluate
+    dict_statistics = {
+        'mean': np.mean,
+        'variance': np.var,
+        'standard deviation': np.std,
+        'max': np.max,
+        'min': np.min,
+        'sum': np.sum,
+    }
+
+    # Create the dictionary with the statistics of the list
+    calculations = dict()
+    for name, function in dict_statistics.items():
+        calculations[name] = [
+            function(np_matrix, axis=0).tolist(),
+            function(np_matrix, axis=1).tolist(),
+            function(np_matrix).tolist()]
+    return calculations
